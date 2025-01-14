c1b5bfd489267bbb86322ebf017a91f11cece803
sebgrgs <sebastien.georges11@gmail.com>
2025-01-14 12:03:15 +0100

    Add script to print all unique combinations of two digits

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i != j:
+            if i != 8 or j != 9:
+                print(f"{i}{j}", end=", ")
+            else:
+                print(f"{i}{j}")
