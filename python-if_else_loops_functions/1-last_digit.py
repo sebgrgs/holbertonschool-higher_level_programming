f794f746318cbded6efaf693f8b24785434da7ad
sebgrgs <sebastien.georges11@gmail.com>
2025-01-14 11:04:56 +0100

    Fix formatting and add missing newlines in Python scripts and README files

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+last = number % 10
 
 if number < 0:
-	last_digit = -last_digit
-if last_digit > 5:
-	print(f"Last digit of {number} is {last_digit} and is greater that 5")
-elif last_digit == 0:
-	print(f"Last digit of {number} is {last_digit} and is zero")
+    last = -last
+if last > 5:
+    print(f"Last digit of {number} is {last} and is greater that 5")
+elif last == 0:
+    print(f"Last digit of {number} is {last} and is zero")
 else:
-	print(f"Last digit of {number} is {last_digit} and is less than 6 and not zero")
+    print(f"Last digit of {number} is {last} and is less than 6 and not zero")
