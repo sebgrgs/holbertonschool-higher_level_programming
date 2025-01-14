5afe962b959a3ca39a4dcaef1569fba02fdba50e
sebgrgs <sebastien.georges11@gmail.com>
2025-01-14 14:04:34 +0100

    Fix output messages and improve function formatting in multiple files

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ last = abs(number) % 10
 if number < 0:
     last = -last
 if last > 5:
-    print(f"Last digit of {number} is {last} and is greater that 5")
+    print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
     print(f"Last digit of {number} is {last} and is 0")
 else:
