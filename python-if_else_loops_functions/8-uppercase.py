48a0d6755a2dfe71e3cf1451edf44c6c7bdc440c
sebgrgs <sebastien.georges11@gmail.com>
2025-01-14 14:38:06 +0100

    Refactor string formatting in multiple print functions for consistency

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):
             char = chr(ord(char) - 32)
-        print(char, end="")
-    print()
+        result += char
+    print("{}".format(result))
