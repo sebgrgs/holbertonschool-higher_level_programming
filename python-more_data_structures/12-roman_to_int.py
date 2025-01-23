03ad39a60e55d540bff458fd3349daf47257bd90
sebgrgs <sebastien.georges11@gmail.com>
2025-01-23 14:05:37 +0100

    Refactor roman_to_int function for improved readability and consistency

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if roman_string == None or type(roman_string) != str:
+    if roman_string is None or type(roman_string) is not str:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     for i in range(len(roman_string)):
-        if roman_string[i] in roman_dict:
-            if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-                result += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
+        if roman_string[i] in dict:
+            if i > 0 and dict[roman_string[i]] > dict[roman_string[i - 1]]:
+                result += dict[roman_string[i]] - 2 * dict[roman_string[i - 1]]
             else:
-                result += roman_dict[roman_string[i]]
-    return(result)
+                result += dict[roman_string[i]]
+    return (result)
