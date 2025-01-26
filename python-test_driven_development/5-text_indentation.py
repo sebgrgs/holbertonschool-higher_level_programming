960162f2de40ef3a26813bb0863da106d695e9f6
sebgrgs <sebastien.georges11@gmail.com>
2025-01-26 19:24:01 +0100

    Implement text_indentation function with input validation and add corresponding test cases

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+This module contains a function that prints a text with 2 new lines after each
+of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    This function prints a text with 2 new lines after each of these characters:
+    ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    
+    c = 0
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] in ".?:":
+            print("\n")
+            if c + 1 < len(text):
+                while c + 1 < len(text) and text[c + 1] == ' ':
+                    c += 1
+        c += 1
