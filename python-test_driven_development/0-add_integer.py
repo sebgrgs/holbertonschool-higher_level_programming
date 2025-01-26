2a165ee06f3a754d5fb9dc98f56b61f35a157e41
sebgrgs <sebastien.georges11@gmail.com>
2025-01-26 17:07:21 +0100

    Update docstring for add_integer function to improve clarity and detail

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Module that adds 2 integers
+description
+    add_integer(a, b=98)
+returns
+    sum of a and b"""
+
 
 def add_integer(a, b=98):
-    """Add two integers or floats together"""
+    """Function that adds 2 integers
+    Args:
+        a: integer or float"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
