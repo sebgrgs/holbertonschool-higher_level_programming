44e515c12a7a54322cb3446d82cb65bf9bf534fe
sebgrgs <sebastien.georges11@gmail.com>
2025-01-28 10:44:47 +0100

    Fix typo in module and class docstrings for Square class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines a Square classeababab"""
+"""Module that defines a Square classeabababaaaaaaaaaaaaa"""
 
 
 class Square:
-    """Class that defines a squareababababbababab"""
+    """Class that defines a squareababababbabababaaaaaaaaaaaaaaaaaa"""
     pass
