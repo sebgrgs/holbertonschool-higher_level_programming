0e7f6797406fb4d39eacaa648163e9d1d5df5ece
sebgrgs <sebastien.georges11@gmail.com>
2025-02-12 09:57:33 +0100

    Fix typo in encoding parameter in save_to_json_file function

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Writes an object to a text file using JSON representation"""
-    with open(filename, 'w', encondig='utf-8') as f:
+    with open(filename, 'w', encodig='utf-8') as f:
         f.write(json.dumps(my_obj))
