ebb85708513d4b44fb4950453a56353bc0be028c
sebgrgs <sebastien.georges11@gmail.com>
2025-02-12 10:43:18 +0100

    Add Student class with JSON serialization and deserialization methods

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for student"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    new_dict[key] = getattr(self, key)
+            return new_dict
