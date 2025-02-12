5ac16eea3df456469f7f09a9a61651732420e245
sebgrgs <sebastien.georges11@gmail.com>
2025-02-12 09:55:13 +0100

    Add JSON serialization and deserialization modules with example usage

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for save_to_json_file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+	"""Writes an object to a text file using JSON representation"""
+	with open(filename, 'w') as f:
+		f.write(json.dumps(my_obj))
