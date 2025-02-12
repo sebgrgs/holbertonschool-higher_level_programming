6d9a0173d6224e731a60f8c8fa01a4ae5a0262a1
sebgrgs <sebastien.georges11@gmail.com>
2025-02-12 10:14:22 +0100

    Fix indentation in class_to_json function docstring

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,5 +3,5 @@
 
 
 def class_to_json(obj):
-	"""Returns the dictionary description with simple data structure"""
-	return obj.__dict__
+    """Returns the dictionary description with simple data structure"""
+    return obj.__dict__
