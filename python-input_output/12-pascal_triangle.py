07bd453bd3ab197dc79099e1b9aceb6dbd57a408
sebgrgs <sebastien.georges11@gmail.com>
2025-02-12 15:47:53 +0100

    Update docstring for pascal_triangle function to improve clarity

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,7 +3,7 @@
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal's triangle of n"""
+    """Returns a list of lists of integers"""
     if n <= 0:
         return []
     triangle = [[1]]
