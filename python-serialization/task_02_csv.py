76c155254fe16d7e70a354193180387a2efc5ba9
sebgrgs <sebastien.georges11@gmail.com>
2025-02-13 13:57:37 +0100

    Add CSV to JSON conversion module with example data

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for CSV serialization"""
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    """Converts a CSV file to a JSON file"""
+    with open(filename, "r") as file:
+        reader = csv.DictReader(file)
+        data = list(reader)
+
+    with open("data.json", "w") as file:
+        json.dump(data, file)
+
+    return data
