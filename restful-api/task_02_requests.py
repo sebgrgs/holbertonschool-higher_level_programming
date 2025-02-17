5dcfb763d69924e4a640e063230005ded7b3ada0
sebgrgs <sebastien.georges11@gmail.com>
2025-02-17 16:18:45 +0100

    Fix capitalization of "Status Code" in fetch_and_print_posts output

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,7 +7,7 @@ import csv
 def fetch_and_print_posts():
     """Fetches and prints posts"""
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"Status code: {response.status_code}")
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
