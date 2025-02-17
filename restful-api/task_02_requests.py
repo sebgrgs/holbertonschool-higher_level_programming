992c395a1f04894683acae631cb331ffa210d97f
sebgrgs <sebastien.georges11@gmail.com>
2025-02-17 16:13:28 +0100

    Refactor fetch_and_save_posts to use DictWriter for CSV output and simplify data extraction

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -19,9 +19,10 @@ def fetch_and_save_posts():
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     if response.status_code == 200:
         posts = response.json()
+
+        posts_data = [{"id": post["id"], "title": post["title"],
+                      "body": post["body"]} for post in posts]
         with open("posts.csv", "w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow(["userId", "id", "title", "body"])
-            for post in posts:
-                writer.writerow([post["userId"], post["id"],
-                                post["title"], post["body"]])
+            writer = csv.DictWriter(f, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(posts_data)
