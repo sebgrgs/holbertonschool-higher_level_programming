156619977e9f938f7c7198c885043f49d90375c0
sebgrgs <sebastien.georges11@gmail.com>
2025-02-18 14:52:43 +0100

    Remove '/info' endpoint from SimpleHTTPRequestHandler in HTTP server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -28,15 +28,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Ok")
-        elif self.path == '/info':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            info = {
-                "version": "1.0",
-                "description": "A simple API built with http.server module"
-            }
-            self.wfile.write(json.dumps(info).encode())
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
