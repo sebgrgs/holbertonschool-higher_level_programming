96e756c65615a2c81b8d687194d00952c5bdd10f
sebgrgs <sebastien.georges11@gmail.com>
2025-03-06 15:07:58 +0100

    Update SQL query in my_filter_states.py to use parameterized input for state name

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
         port=3306
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` \
-    WHERE `name` LIKE 'N%' \
-    ORDER BY `id` ASC")
+    query = "SELECT * FROM states WHERE name = '{}' \
+             ORDER BY `id` ASC".format(sys.argv[4])
+    cursor.execute(query)
     rows = cursor.fetchall()
 
     for row in rows:
